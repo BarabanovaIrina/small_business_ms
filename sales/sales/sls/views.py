c65352c94e2bb09aef00c15f9e6833d3f6cd577c
BarabanovaIrina <irlania@mail.ru>
2023-05-03 14:06:24 +0300

    Add Postgres database support for Sales service
    - Extend `sales.sales.sls.views.py` to process CRUD operations
    on Orders
    - Add `Order` and `OrderDetail` models
    - Modify `sales.sales.sls.services.py` to work with Models
    - Add Postgres configurations to `sales.docker-compose.yml`
    to run locally.

--- sales/sales/sls/views.py
+++ sales/sales/sls/views.py
@@ -1,20 +1,73 @@
+import json
+
 import requests
-from django.http import JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.views import View
 
-from .services import all_orders, get_order
+from .services import (
+    all_orders,
+    create_order,
+    delete_order,
+    get_order_by_id,
+    update_order,
+)
 
 
 class OrdersView(View):
     def get(self, request):
         orders = all_orders()
-        return JsonResponse(orders)
+        return HttpResponse(
+            json.dumps([order.dict() for order in orders]),
+            content_type="application/json",
+        )
+
+    def post(self, request):
+        """
+        Reqest Example:
+        {
+            "customer_id": 1,
+            "items": [
+                {
+                    "id": 1,
+                    "price": 10.0,
+                    "quantity": 10
+                }
+            ]
+        }
+        """
+        data = json.loads(request.body)
+        customer_id = data.get("customer_id")
+        # TODO add items data validation
+        items = data.get("items")
+        order_id = create_order(
+            customer_id=customer_id,
+            items=items,
+        )
+        return HttpResponse(f"Order with id={order_id} created")
 
 
 class OrderDetail(View):
     def get(self, request, pk: int):
-        product = get_order(pk)
-        return JsonResponse(product)
+        product = get_order_by_id(pk)
+        if product is None:
+            raise Http404("Opps, order not found")
+        return JsonResponse(product.dict())
+
+    def put(self, request, pk: int):
+        data = json.loads(request.body)
+        customer_id = data.get("customer_id")
+        try:
+            update_order(
+                pk=pk,
+                customer_id=customer_id,
+            )
+        except ValueError:
+            raise Http404("Opps, order not found")
+        return HttpResponse(f"Order {pk} updated")
+
+    def delete(self, request, pk: int):
+        delete_order(pk)
+        return HttpResponse(f"Order {pk} deleted")
 
 
 class SoldItemsView(View):
--- sales/sales/tests/sls/test_views.py
+++ sales/sales/tests/sls/test_views.py
@@ -1,45 +1,107 @@
 import json
-from unittest.mock import MagicMock, patch
+from datetime import datetime
+from unittest.mock import patch
 
 from django.test import TestCase
 
+from sales.sls.data_models import OrderData, OrderedItemData
+
 
 class TestSalesOrdersListView(TestCase):
-    @patch("sales.sls.views.all_orders", return_value={"dummy": "dummy"})
+    @patch(
+        "sales.sls.views.all_orders",
+        return_value=[
+            OrderData(
+                id=1,
+                customer_id=1,
+                date=datetime(2023, 5, 2),
+                items=[OrderedItemData(id=1, price=10.0, quantity=5)],
+            )
+        ],
+    )
     def test_all_orders_returns_200(self, all_orders_mock):
         response = self.client.get("/sales/orders")
         all_orders_mock.assert_called_once()
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {"dummy": "dummy"})
+        self.assertEqual(
+            json.loads(response.content),
+            [
+                {
+                    "id": 1,
+                    "customer_id": 1,
+                    "date": "02/05/2023, 00:00:00",
+                    "items": [{"id": 1, "price": 10.0, "quantity": 5}],
+                }
+            ],
+        )
+
+    @patch("sales.sls.views.create_order", return_value=1)
+    def test_create_order(self, create_order_mock):
+        response = self.client.post(
+            "/sales/orders",
+            data={
+                "customer_id": 1,
+                "items": [{"id": 1, "price": 10.0, "quantity": 10}],
+            },
+            content_type="application/json",
+        )
+        create_order_mock.assert_called_once()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Order with id=1 created")
 
 
 class TestOrderDetailView(TestCase):
     @patch(
-        "sales.sls.views.get_order",
-        return_value={"transaction_id": "transaction_1", "user_id": "user_1"},
+        "sales.sls.views.get_order_by_id",
+        return_value=OrderData(
+            id=1,
+            customer_id=1,
+            date=datetime(2023, 5, 2),
+            items=[OrderedItemData(id=1, price=10.0, quantity=5)],
+        ),
     )
     def test_get_order_returns_200(self, get_order_mock):
         response = self.client.get("/sales/orders/1")
         get_order_mock.assert_called_once()
         self.assertEqual(response.status_code, 200)
 
+    @patch("sales.sls.views.get_order_by_id", return_value=None)
+    def test_get_transaction_returns_404(self, get_transaction_mock):
+        response = self.client.get("/sales/orders/1")
+        get_transaction_mock.assert_called_once()
+        self.assertEqual(response.status_code, 404)
 
-class TestSoldItemsView(TestCase):
-    @patch("sales.sls.views.requests")
-    def test(self, request_mock):
-        # given
-        response_mock = MagicMock()
-        response_mock.status_code = 200
-        response_mock.json.return_value = {
-            "product_name": "name_1",
-            "quantity": 1,
-            "price": 10,
-        }
-        request_mock.get.return_value = response_mock
-
-        # when
-        response = self.client.get("/sales/sold_items")
-
-        # then
-        request_mock.get.assert_called_once()
+    @patch("sales.sls.views.update_order")
+    def test_update_order(self, update_order_mock):
+        response = self.client.put("/sales/orders/1", data=b'{"customer_id": "327"}')
+        update_order_mock.assert_called_once()
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Order 1 updated")
+
+    @patch("sales.sls.views.delete_order")
+    def test_delete_order(self, delete_mock):
+        response = self.client.delete("/sales/orders/1")
+        delete_mock.assert_called_once()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b"Order 1 deleted")
+
+
+# class TestSoldItemsView(TestCase):
+#     @patch("sales.sls.views.requests")
+#     def test(self, request_mock):
+#         # given
+#         response_mock = MagicMock()
+#         response_mock.status_code = 200
+#         response_mock.json.return_value = {
+#             "product_name": "name_1",
+#             "quantity": 1,
+#             "price": 10,
+#         }
+#         request_mock.get.return_value = response_mock
+#
+#         # when
+#         response = self.client.get("/sales/sold_items")
+#
+#         # then
+#         request_mock.get.assert_called_once()
+#         self.assertEqual(response.status_code, 200)
